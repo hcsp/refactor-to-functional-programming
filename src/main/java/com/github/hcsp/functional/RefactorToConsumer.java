5e82bc50f974016a9057dcf7deb24aa3fd80c2e0
magicP <icanmagic@163.com>
2019-11-24 21:09:22 +0800

    使用函数式重构代码

--- src/main/java/com/github/hcsp/functional/RefactorToConsumer.java
+++ src/main/java/com/github/hcsp/functional/RefactorToConsumer.java
@@ -11,59 +11,21 @@ public class RefactorToConsumer {
                 Stream.of("a", "b", "c").collect(Collectors.toMap(k -> k, v -> v));
         Map<String, String> map2 =
                 Stream.of("d", "e", "f").collect(Collectors.toMap(k -> k, v -> v));
-
-        printWithComma(map1, map2);
-        printWithDash(map1, map2);
-        printWithColon(map1, map2);
+        String[] separators = {",", "-", ":"};
+        for (String separator : separators) {
+            printWithConsumer(map1, map2, generateBiConsumer(separator));
+        }
     }
 
-    // 请尝试使用BiConsumer函数式接口重构下列三个方法，消除重复代码，提高可读性
-    // 提示：你可以使用Map.forEach方法
-    // 加分项：如果你能编写一个返回BiConsumer的高阶函数（即"返回函数的函数"），那就更好了
     public static void printWithConsumer(
             Map<String, String> map1,
             Map<String, String> map2,
-            BiConsumer<String, String> consumer) {}
-
-    public static void printWithComma(Map<String, String> map1, Map<String, String> map2) {
-        for (Map.Entry<String, String> entry : map1.entrySet()) {
-            String key = entry.getKey();
-            String value = entry.getValue();
-            System.out.println(key + "," + value);
-        }
-
-        for (Map.Entry<String, String> entry : map2.entrySet()) {
-            String key = entry.getKey();
-            String value = entry.getValue();
-            System.out.println(key + "," + value);
-        }
+            BiConsumer<String, String> consumer) {
+        map1.forEach(consumer);
+        map2.forEach(consumer);
     }
 
-    public static void printWithDash(Map<String, String> map1, Map<String, String> map2) {
-        for (Map.Entry<String, String> entry : map1.entrySet()) {
-            String key = entry.getKey();
-            String value = entry.getValue();
-            System.out.println(key + "-" + value);
-        }
-
-        for (Map.Entry<String, String> entry : map2.entrySet()) {
-            String key = entry.getKey();
-            String value = entry.getValue();
-            System.out.println(key + "-" + value);
-        }
-    }
-
-    public static void printWithColon(Map<String, String> map1, Map<String, String> map2) {
-        for (Map.Entry<String, String> entry : map1.entrySet()) {
-            String key = entry.getKey();
-            String value = entry.getValue();
-            System.out.println(key + ":" + value);
-        }
-
-        for (Map.Entry<String, String> entry : map2.entrySet()) {
-            String key = entry.getKey();
-            String value = entry.getValue();
-            System.out.println(key + ":" + value);
-        }
+    public static BiConsumer<String, String> generateBiConsumer(String separator) {
+        return (key, value) -> System.out.println(key + separator + value);
     }
 }
