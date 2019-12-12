548ddb775e663b9193ab07917c4cc3007c2645eb
shenwl <shenweilun96@gmail.com>
2019-12-12 23:19:19 +0800

    使用函数式重构代码 (#32)
    使用函数式重构代码 (#32)

--- src/main/java/com/github/hcsp/functional/RefactorToSupplier.java
+++ src/main/java/com/github/hcsp/functional/RefactorToSupplier.java
@@ -10,6 +10,8 @@ public class RefactorToSupplier {
         return new Random().nextInt();
     }
 
+    private static int currentStringIndex = 0;
+
     public static void main(String[] args) {
         System.out.println(createObjects());
         System.out.println(createStrings());
@@ -19,30 +21,29 @@ public class RefactorToSupplier {
     // 请尝试使用函数式接口Supplier对三个方法进行重构，消除冗余代码
     // 并尽量尝试使用lambda表达式和方法引用来传递参数
     public static List<Object> create(Supplier<Object> supplier) {
-        return null;
+        List<Object> result = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            result.add(supplier.get());
+        }
+        return result;
+    }
+
+    public static String createString() {
+        String str = "" + currentStringIndex;
+        currentStringIndex++;
+        return str;
     }
 
     public static List<Object> createObjects() {
-        List<Object> result = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            result.add(new Object());
-        }
-        return result;
+        return create(Object::new);
     }
 
     public static List<Object> createStrings() {
-        List<Object> result = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            result.add("" + i);
-        }
-        return result;
+        return create(RefactorToSupplier::createString);
     }
 
     public static List<Object> createRandomIntegers() {
-        List<Object> result = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            result.add(randomInt());
-        }
-        return result;
+        return create(RefactorToSupplier::randomInt);
     }
 }
