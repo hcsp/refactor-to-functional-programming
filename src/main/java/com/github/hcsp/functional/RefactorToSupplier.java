7c5d841571c73666ff386d8cc250f1b680d4b42d
87244099 <872440899@qq.com>
2020-01-01 10:20:05 +0800

    优化代码

--- src/main/java/com/github/hcsp/functional/RefactorToSupplier.java
+++ src/main/java/com/github/hcsp/functional/RefactorToSupplier.java
@@ -1,9 +1,12 @@
 package com.github.hcsp.functional;
 
-import java.util.ArrayList;
+
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class RefactorToSupplier {
     private static int randomInt() {
@@ -19,30 +22,21 @@ public class RefactorToSupplier {
     // 请尝试使用函数式接口Supplier对三个方法进行重构，消除冗余代码
     // 并尽量尝试使用lambda表达式和方法引用来传递参数
     public static List<Object> create(Supplier<Object> supplier) {
-        return null;
+        //去掉循环
+        return IntStream.range(0, 10).mapToObj((i)->supplier.get()).collect(Collectors.toList());
     }
 
     public static List<Object> createObjects() {
-        List<Object> result = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            result.add(new Object());
-        }
-        return result;
+        return create(Object::new);
     }
 
     public static List<Object> createStrings() {
-        List<Object> result = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            result.add("" + i);
-        }
-        return result;
+        //外部弄一个变量自增，传递进去。。。
+        AtomicInteger integer = new AtomicInteger(0);
+        return create(()->integer.getAndIncrement() + "");
     }
 
     public static List<Object> createRandomIntegers() {
-        List<Object> result = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            result.add(randomInt());
-        }
-        return result;
+        return create(RefactorToSupplier::randomInt);
     }
 }
