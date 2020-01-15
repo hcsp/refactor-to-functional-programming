3cd71c00400142689ca57f0dbc208bb949c14d45
zaker-666 <1075151259china@gmail.com>
2020-01-15 16:30:35 +0800

    got it

--- src/main/java/com/github/hcsp/functional/RefactorToFunction.java
+++ src/main/java/com/github/hcsp/functional/RefactorToFunction.java
@@ -8,50 +8,24 @@ public class RefactorToFunction {
         int[] a = {4, 5, 6};
         int[] b = {1, 2, 3};
 
-        System.out.println(Arrays.toString(add(a, b)));
-        System.out.println(Arrays.toString(minus(a, b)));
-        System.out.println(Arrays.toString(multiply(a, b)));
-        System.out.println(Arrays.toString(divide(a, b)));
+        System.out.println(Arrays.toString(calculate(a, b, Integer::sum)));
+        System.out.println(Arrays.toString(calculate(a, b, (x1, x2) -> x1 - x2)));
+        System.out.println(Arrays.toString(calculate(a, b, (x1, x2) -> x1 * x2)));
+        System.out.println(Arrays.toString(calculate(a, b, (x1, x2) -> x1 / x2)));
     }
 
     // 请尝试将下列四个方法使用IntBinaryOperator进行重构，减少重复代码
     public static int[] calculate(int[] a, int[] b, IntBinaryOperator operator) {
-        return null;
+        int[] results = new int[a.length];
+        for (int i = 0; i < a.length; i++) {
+            results[i] = operator.applyAsInt(a[i], b[i]);
+        }
+        return results;
     }
 
     // 将两个数组中的每个数字分别相加，然后返回相加后的数组。你可以假定传入的数组都是等长的
     // 下列minus/multiply/divide方法同理
     // 例如，a=[1, 2, 3], b=[4, 5, 6]
     // 返回 [5 (1+4), 7 (2+5), 9 (3+6)]
-    public static int[] add(int[] a, int[] b) {
-        int[] result = new int[a.length];
-        for (int i = 0; i < a.length; ++i) {
-            result[i] = a[i] + b[i];
-        }
-        return result;
-    }
 
-    public static int[] minus(int[] a, int[] b) {
-        int[] result = new int[a.length];
-        for (int i = 0; i < a.length; ++i) {
-            result[i] = a[i] - b[i];
-        }
-        return result;
-    }
-
-    public static int[] multiply(int[] a, int[] b) {
-        int[] result = new int[a.length];
-        for (int i = 0; i < a.length; ++i) {
-            result[i] = a[i] * b[i];
-        }
-        return result;
-    }
-
-    public static int[] divide(int[] a, int[] b) {
-        int[] result = new int[a.length];
-        for (int i = 0; i < a.length; ++i) {
-            result[i] = a[i] / b[i];
-        }
-        return result;
-    }
 }
