9cf4c959813ae11842fc52a235e992b8da17cb7f
zaker-666 <1075151259china@gmail.com>
2020-01-15 17:19:55 +0800

    got it (#37)
    got it (#37)

--- src/main/java/com/github/hcsp/functional/RefactorToSupplier.java
+++ src/main/java/com/github/hcsp/functional/RefactorToSupplier.java
@@ -6,43 +6,25 @@ import java.util.Random;
 import java.util.function.Supplier;
 
 public class RefactorToSupplier {
+    private static int init = 0;
+
     private static int randomInt() {
         return new Random().nextInt();
     }
 
     public static void main(String[] args) {
-        System.out.println(createObjects());
-        System.out.println(createStrings());
-        System.out.println(createRandomIntegers());
+        System.out.println(create(Object::new));
+        System.out.println(create(() -> "" + (init++)));
+        System.out.println(create(RefactorToSupplier::randomInt));
     }
 
     // 请尝试使用函数式接口Supplier对三个方法进行重构，消除冗余代码
     // 并尽量尝试使用lambda表达式和方法引用来传递参数
     public static List<Object> create(Supplier<Object> supplier) {
-        return null;
-    }
-
-    public static List<Object> createObjects() {
-        List<Object> result = new ArrayList<>();
+        List<Object> objects = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
-            result.add(new Object());
+            objects.add(supplier.get());
         }
-        return result;
-    }
-
-    public static List<Object> createStrings() {
-        List<Object> result = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            result.add("" + i);
-        }
-        return result;
-    }
-
-    public static List<Object> createRandomIntegers() {
-        List<Object> result = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            result.add(randomInt());
-        }
-        return result;
+        return objects;
     }
 }
