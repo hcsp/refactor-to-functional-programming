84fe813e84f9a70615bf5a54abb635cfe7854e0f
NervousOrange <zch693922442@outlook.com>
2020-01-22 13:40:03 +0100

    from NervousOrange: Assignment 1 of Functional programming.

--- src/main/java/com/github/hcsp/functional/RefactorToSupplier.java
+++ src/main/java/com/github/hcsp/functional/RefactorToSupplier.java
@@ -19,30 +19,35 @@ public class RefactorToSupplier {
     // 请尝试使用函数式接口Supplier对三个方法进行重构，消除冗余代码
     // 并尽量尝试使用lambda表达式和方法引用来传递参数
     public static List<Object> create(Supplier<Object> supplier) {
-        return null;
+        List<Object> result = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            result.add(supplier.get());
+        }
+        return result;
     }
 
     public static List<Object> createObjects() {
-        List<Object> result = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            result.add(new Object());
-        }
-        return result;
+        return create(Object::new);
     }
 
     public static List<Object> createStrings() {
-        List<Object> result = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            result.add("" + i);
+        IncreasingInt increasingInt = new IncreasingInt(-1);
+        return create(increasingInt::add);
+    }
+
+    static class IncreasingInt {
+        Integer integer;
+
+        IncreasingInt(Integer integer) {
+            this.integer = integer;
         }
-        return result;
+
+        int add() {
+            return this.integer = integer + 1;
+        }
     }
 
     public static List<Object> createRandomIntegers() {
-        List<Object> result = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            result.add(randomInt());
-        }
-        return result;
+        return create(RefactorToSupplier::randomInt);
     }
 }
