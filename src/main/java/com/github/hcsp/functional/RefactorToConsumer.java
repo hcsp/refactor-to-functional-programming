4a4bd0f33f3c702e6846abaa2b42f3a8e7e70be6
feiyongjing <ysyhljt>
2020-02-13 21:01:46 +0800

    使用函数式重构代码

--- src/main/java/com/github/hcsp/functional/RefactorToConsumer.java
+++ src/main/java/com/github/hcsp/functional/RefactorToConsumer.java
@@ -1,5 +1,6 @@
 package com.github.hcsp.functional;
 
+import java.util.List;
 import java.util.Map;
 import java.util.function.BiConsumer;
 import java.util.stream.Collectors;
@@ -17,53 +18,26 @@ public class RefactorToConsumer {
         printWithColon(map1, map2);
     }
 
-    // 请尝试使用BiConsumer函数式接口重构下列三个方法，消除重复代码，提高可读性
+    // 请尝试使用BiConsumer函数式接口重构下列三个方法，消除重复代码，提高可读性0
     // 提示：你可以使用Map.forEach方法
     // 加分项：如果你能编写一个返回BiConsumer的高阶函数（即"返回函数的函数"），那就更好了
     public static void printWithConsumer(
             Map<String, String> map1,
             Map<String, String> map2,
-            BiConsumer<String, String> consumer) {}
+            BiConsumer<String, String> consumer) {
+            map1.forEach(consumer);
+            map2.forEach(consumer);
 
+    }
     public static void printWithComma(Map<String, String> map1, Map<String, String> map2) {
-        for (Map.Entry<String, String> entry : map1.entrySet()) {
-            String key = entry.getKey();
-            String value = entry.getValue();
-            System.out.println(key + "," + value);
-        }
-
-        for (Map.Entry<String, String> entry : map2.entrySet()) {
-            String key = entry.getKey();
-            String value = entry.getValue();
-            System.out.println(key + "," + value);
-        }
+        printWithConsumer(map1, map2, (key, value) -> System.out.println(key + "," + value));
     }
 
     public static void printWithDash(Map<String, String> map1, Map<String, String> map2) {
-        for (Map.Entry<String, String> entry : map1.entrySet()) {
-            String key = entry.getKey();
-            String value = entry.getValue();
-            System.out.println(key + "-" + value);
-        }
-
-        for (Map.Entry<String, String> entry : map2.entrySet()) {
-            String key = entry.getKey();
-            String value = entry.getValue();
-            System.out.println(key + "-" + value);
-        }
+        printWithConsumer(map1, map2, (key, value) -> System.out.println(key + "-" + value));
     }
 
     public static void printWithColon(Map<String, String> map1, Map<String, String> map2) {
-        for (Map.Entry<String, String> entry : map1.entrySet()) {
-            String key = entry.getKey();
-            String value = entry.getValue();
-            System.out.println(key + ":" + value);
-        }
-
-        for (Map.Entry<String, String> entry : map2.entrySet()) {
-            String key = entry.getKey();
-            String value = entry.getValue();
-            System.out.println(key + ":" + value);
-        }
+        printWithConsumer(map1, map2, (key, value) -> System.out.println(key + ":" + value));
     }
 }
