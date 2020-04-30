595fcec1aeb5b4f2f8e6b83240ad54f054d1ea92
Sunpeng526 <55687152+Sunpeng526@users.noreply.github.com>
2020-04-30 09:48:33 +0800

    refactor to functional programming (#78)
    refactor to functional programming (#78)

--- src/main/java/com/github/hcsp/functional/RefactorToSupplier.java
+++ src/main/java/com/github/hcsp/functional/RefactorToSupplier.java
@@ -6,20 +6,39 @@ import java.util.Random;
 import java.util.function.Supplier;
 
 public class RefactorToSupplier {
+    private static int count = 0;
+
     private static int randomInt() {
         return new Random().nextInt();
     }
 
+    private static String IncreaseInteger() {
+        return "" + count++;
+    }
+
     public static void main(String[] args) {
-        System.out.println(createObjects());
+        /* create objects */
+        Supplier<Object> createObjects = () -> new Object();
+        System.out.println(create(createObjects));
+
+        /* Create Strings */
+        Supplier<Object> createStrings = () -> IncreaseInteger();
         System.out.println(createStrings());
-        System.out.println(createRandomIntegers());
+        System.out.println(create(createStrings));
+
+        /* Create Integers */
+        Supplier<Object> createRandomIntegers = () -> randomInt();
+        System.out.println(create(createRandomIntegers));
     }
 
     // 请尝试使用函数式接口Supplier对三个方法进行重构，消除冗余代码
     // 并尽量尝试使用lambda表达式和方法引用来传递参数
     public static List<Object> create(Supplier<Object> supplier) {
-        return null;
+        List<Object> result = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            result.add(supplier.get());
+        }
+        return result;
     }
 
     public static List<Object> createObjects() {
