12af8fe670dce46f953200a1666e2864ed8e742d
kongmu373 <62830983+kongmu373@users.noreply.github.com>
2020-06-07 10:45:53 +0800

    使用函数式重构代码 (#87)
    使用函数式重构代码 (#87)

--- src/main/java/com/github/hcsp/functional/RefactorToConsumer.java
+++ src/main/java/com/github/hcsp/functional/RefactorToConsumer.java
@@ -8,9 +8,9 @@ import java.util.stream.Stream;
 public class RefactorToConsumer {
     public static void main(String[] args) {
         Map<String, String> map1 =
-                Stream.of("a", "b", "c").collect(Collectors.toMap(k -> k, v -> v));
+            Stream.of("a", "b", "c").collect(Collectors.toMap(k -> k, v -> v));
         Map<String, String> map2 =
-                Stream.of("d", "e", "f").collect(Collectors.toMap(k -> k, v -> v));
+            Stream.of("d", "e", "f").collect(Collectors.toMap(k -> k, v -> v));
 
         printWithComma(map1, map2);
         printWithDash(map1, map2);
@@ -21,49 +21,22 @@ public class RefactorToConsumer {
     // 提示：你可以使用Map.forEach方法
     // 加分项：如果你能编写一个返回BiConsumer的高阶函数（即"返回函数的函数"），那就更好了
     public static void printWithConsumer(
-            Map<String, String> map1,
-            Map<String, String> map2,
-            BiConsumer<String, String> consumer) {}
+        Map<String, String> map1,
+        Map<String, String> map2,
+        BiConsumer<String, String> consumer) {
+        map1.forEach(consumer);
+        map2.forEach(consumer);
+    }
 
     public static void printWithComma(Map<String, String> map1, Map<String, String> map2) {
-        for (Map.Entry<String, String> entry : map1.entrySet()) {
-            String key = entry.getKey();
-            String value = entry.getValue();
-            System.out.println(key + "," + value);
-        }
-
-        for (Map.Entry<String, String> entry : map2.entrySet()) {
-            String key = entry.getKey();
-            String value = entry.getValue();
-            System.out.println(key + "," + value);
-        }
+        printWithConsumer(map1, map2, (k, v) -> System.out.println(k + "," + v));
     }
 
     public static void printWithDash(Map<String, String> map1, Map<String, String> map2) {
-        for (Map.Entry<String, String> entry : map1.entrySet()) {
-            String key = entry.getKey();
-            String value = entry.getValue();
-            System.out.println(key + "-" + value);
-        }
-
-        for (Map.Entry<String, String> entry : map2.entrySet()) {
-            String key = entry.getKey();
-            String value = entry.getValue();
-            System.out.println(key + "-" + value);
-        }
+        printWithConsumer(map1, map2, (k, v) -> System.out.println(k + "-" + v));
     }
 
     public static void printWithColon(Map<String, String> map1, Map<String, String> map2) {
-        for (Map.Entry<String, String> entry : map1.entrySet()) {
-            String key = entry.getKey();
-            String value = entry.getValue();
-            System.out.println(key + ":" + value);
-        }
-
-        for (Map.Entry<String, String> entry : map2.entrySet()) {
-            String key = entry.getKey();
-            String value = entry.getValue();
-            System.out.println(key + ":" + value);
-        }
+        printWithConsumer(map1, map2, (k, v) -> System.out.println(k + ":" + v));
     }
 }
