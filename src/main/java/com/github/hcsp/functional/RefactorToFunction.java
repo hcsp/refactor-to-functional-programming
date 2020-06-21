0e50141363d962b6083b9156b11ef3ce5be7786b
Haixiang <haixiang6123@gmail.com>
2020-06-21 10:46:30 +0800

    使用函数式重构代码

--- src/main/java/com/github/hcsp/functional/RefactorToFunction.java
+++ src/main/java/com/github/hcsp/functional/RefactorToFunction.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.function.IntBinaryOperator;
 
 public class RefactorToFunction {
+
     public static void main(String[] args) {
         int[] a = {4, 5, 6};
         int[] b = {1, 2, 3};
@@ -16,7 +17,11 @@ public class RefactorToFunction {
 
     // 请尝试将下列四个方法使用IntBinaryOperator进行重构，减少重复代码
     public static int[] calculate(int[] a, int[] b, IntBinaryOperator operator) {
-        return null;
+        int[] result = new int[a.length];
+        for (int i = 0; i < a.length; i++) {
+            result[i] = operator.applyAsInt(a[i], b[i]);
+        }
+        return result;
     }
 
     // 将两个数组中的每个数字分别相加，然后返回相加后的数组。你可以假定传入的数组都是等长的
@@ -24,34 +29,18 @@ public class RefactorToFunction {
     // 例如，a=[1, 2, 3], b=[4, 5, 6]
     // 返回 [5 (1+4), 7 (2+5), 9 (3+6)]
     public static int[] add(int[] a, int[] b) {
-        int[] result = new int[a.length];
-        for (int i = 0; i < a.length; ++i) {
-            result[i] = a[i] + b[i];
-        }
-        return result;
+        return calculate(a, b, Integer::sum);
     }
 
     public static int[] minus(int[] a, int[] b) {
-        int[] result = new int[a.length];
-        for (int i = 0; i < a.length; ++i) {
-            result[i] = a[i] - b[i];
-        }
-        return result;
+        return calculate(a, b, (v1, v2) -> v1 - v2);
     }
 
     public static int[] multiply(int[] a, int[] b) {
-        int[] result = new int[a.length];
-        for (int i = 0; i < a.length; ++i) {
-            result[i] = a[i] * b[i];
-        }
-        return result;
+        return calculate(a, b, (v1, v2) -> v1 * v2);
     }
 
     public static int[] divide(int[] a, int[] b) {
-        int[] result = new int[a.length];
-        for (int i = 0; i < a.length; ++i) {
-            result[i] = a[i] / b[i];
-        }
-        return result;
+        return calculate(a, b, (v1, v2) -> v1 / v2);
     }
 }
