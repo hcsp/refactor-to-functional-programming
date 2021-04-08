62b154b7946995ffde18b0e29dade958e8a321a3
cbx <2943189638@qq.com>
2021-04-08 20:04:40 +0800

    提交作业

--- src/main/java/com/github/hcsp/functional/RefactorToSupplier.java
+++ src/main/java/com/github/hcsp/functional/RefactorToSupplier.java
@@ -11,21 +11,20 @@ public class RefactorToSupplier {
     }
 
     public static void main(String[] args) {
-        System.out.println(createObjects());
-        System.out.println(createStrings());
-        System.out.println(createRandomIntegers());
+//        System.out.println(createObjects());
+//        System.out.println(createStrings());
+//        System.out.println(createRandomIntegers());
+        create(Object::new);
+        create(RefactorToSupplier::createStrings);
+        create(RefactorToSupplier::randomInt);
     }
 
     // 请尝试使用函数式接口Supplier对三个方法进行重构，消除冗余代码
     // 并尽量尝试使用lambda表达式和方法引用来传递参数
     public static List<Object> create(Supplier<Object> supplier) {
-        return null;
-    }
-
-    public static List<Object> createObjects() {
         List<Object> result = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
-            result.add(new Object());
+            result.add(supplier.get());
         }
         return result;
     }
@@ -38,11 +37,27 @@ public class RefactorToSupplier {
         return result;
     }
 
-    public static List<Object> createRandomIntegers() {
-        List<Object> result = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            result.add(randomInt());
-        }
-        return result;
-    }
+//    public static List<Object> createObjects() {
+//        List<Object> result = new ArrayList<>();
+//        for (int i = 0; i < 10; i++) {
+//            result.add(new Object());
+//        }
+//        return result;
+//    }
+
+//    public static List<Object> createStrings() {
+//        List<Object> result = new ArrayList<>();
+//        for (int i = 0; i < 10; i++) {
+//            result.add("" + i);
+//        }
+//        return result;
+//    }
+
+//    public static List<Object> createRandomIntegers() {
+//        List<Object> result = new ArrayList<>();
+//        for (int i = 0; i < 10; i++) {
+//            result.add(randomInt());
+//        }
+//        return result;
+//    }
 }
