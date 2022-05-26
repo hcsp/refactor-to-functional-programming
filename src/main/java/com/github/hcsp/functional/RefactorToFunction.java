a701c369fcfd01cb9a1d24c2ebfd8304ea6f195a
LiuXing <94578246+iamxliu@users.noreply.github.com>
2022-05-26 15:07:12 +0800

    函数编程 (#145)
    函数编程 (#145)

--- src/main/java/com/github/hcsp/functional/RefactorToFunction.java
+++ src/main/java/com/github/hcsp/functional/RefactorToFunction.java
@@ -16,7 +16,15 @@ public class RefactorToFunction {
 
     // 请尝试将下列四个方法使用IntBinaryOperator进行重构，减少重复代码
     public static int[] calculate(int[] a, int[] b, IntBinaryOperator operator) {
-        return null;
+
+        int[] result = new int[a.length];
+        for (int i = 0; i < a.length; i++) {
+            result[i] = operator.applyAsInt(a[i], b[i]);
+
+
+        }
+        return result;
+
     }
 
     // 将两个数组中的每个数字分别相加，然后返回相加后的数组。你可以假定传入的数组都是等长的
@@ -24,34 +32,19 @@ public class RefactorToFunction {
     // 例如，a=[1, 2, 3], b=[4, 5, 6]
     // 返回 [5 (1+4), 7 (2+5), 9 (3+6)]
     public static int[] add(int[] a, int[] b) {
-        int[] result = new int[a.length];
-        for (int i = 0; i < a.length; ++i) {
-            result[i] = a[i] + b[i];
-        }
-        return result;
+//        return calculate(a, b, (i, j) -> i + j);
+        return calculate(a, b, Integer::sum);
     }
 
     public static int[] minus(int[] a, int[] b) {
-        int[] result = new int[a.length];
-        for (int i = 0; i < a.length; ++i) {
-            result[i] = a[i] - b[i];
-        }
-        return result;
+        return calculate(a, b, (i, j) -> i - j);
     }
 
     public static int[] multiply(int[] a, int[] b) {
-        int[] result = new int[a.length];
-        for (int i = 0; i < a.length; ++i) {
-            result[i] = a[i] * b[i];
-        }
-        return result;
+        return calculate(a, b, (i, j) -> i * j);
     }
 
     public static int[] divide(int[] a, int[] b) {
-        int[] result = new int[a.length];
-        for (int i = 0; i < a.length; ++i) {
-            result[i] = a[i] / b[i];
-        }
-        return result;
+        return calculate(a, b, (i, j) -> i / j);
     }
 }
